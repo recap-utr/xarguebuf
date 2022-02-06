5301c462e2562ddfe4423e37002bede964806161
Mirko Lenz <info@mirko-lenz.de>
2022-02-06 17:18:50 +0100

    Add helper functions

--- twitter2arguebuf/__main__.py
+++ twitter2arguebuf/__main__.py
@@ -1,11 +1,11 @@
 import typer
 
-from . import convert, download, parse_annotations
+from . import api, convert, parse_annotations
 
 app = typer.Typer()
 
 app.command()(convert.convert)
-app.command()(download.download)
+app.add_typer(api.app, name="api")
 app.add_typer(parse_annotations.app, name="parse-annotations")
 
 if __name__ == "__main__":
