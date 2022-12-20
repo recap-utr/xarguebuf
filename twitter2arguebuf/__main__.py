1f0b86ad5d3642cb66f25fbbdd503418d93d6c43
Mirko Lenz <mirko@mirkolenz.com>
2022-12-21 00:31:01 +0100

    fix!: remove annotations parser
    Not needed anymore because arguemapper is ready

--- twitter2arguebuf/__main__.py
+++ twitter2arguebuf/__main__.py
@@ -1,12 +1,11 @@
 import typer
 
-from . import convert, count, parse_annotations
+from . import convert, count
 
 app = typer.Typer()
 
 app.command()(convert.convert)
 app.command()(count.count)
-app.add_typer(parse_annotations.app, name="parse-annotations")
 
 if __name__ == "__main__":
     app()
