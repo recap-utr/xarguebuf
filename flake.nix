7fb6283235d49fe98c37836752f7932928352f97
Mirko Lenz <mirko@mirkolenz.com>
2023-05-25 22:32:06 +0200

    build: add nix flake

--- flake.nix
+++ flake.nix
@@ -12,14 +12,9 @@
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { pkgs, system, lib, self', ... }:
         let
-          inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication mkPoetryEnv;
-          poetryArgs = {
-            projectDir = ./.;
-            preferWheels = true;
-            python = pkgs.python311;
-          };
-          venv = mkPoetryEnv poetryArgs;
-          app = mkPoetryApplication poetryArgs;
+          inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+          python = pkgs.python311;
+          poetry = pkgs.poetry;
         in
         {
           apps.copyDockerImage = {
@@ -28,26 +23,36 @@
               IFS=$'\n' # iterate over newlines
               set -x # echo on
               for DOCKER_TAG in $DOCKER_METADATA_OUTPUT_TAGS; do
-                ${lib.getExe pkgs.skopeo} copy "docker-archive:${self'.packages.dockerImage}" "docker://$DOCKER_TAG"
+                ${lib.getExe pkgs.skopeo} --insecure-policy copy "docker-archive:${self'.packages.dockerImage}" "docker://$DOCKER_TAG"
               done
             '');
           };
-          packages = {
-            twitter2arguebuf = app;
-            default = app;
-            dockerImage = pkgs.dockerTools.buildImage {
-              name = "twitter2arguebuf";
-              config = {
-                entrypoint = [ (lib.getExe app) ];
-                cmd = [ "--help" ];
+          packages =
+            let
+              app = mkPoetryApplication {
+                inherit python;
+                projectDir = ./.;
+                preferWheels = true;
+              };
+            in
+            {
+              twitter2arguebuf = app;
+              default = app;
+              dockerImage = pkgs.dockerTools.buildImage {
+                name = "twitter2arguebuf";
+                config = {
+                  entrypoint = [ (lib.getExe app) ];
+                  cmd = [ "--help" ];
+                };
               };
             };
-          };
           devShells.default = pkgs.mkShell {
+            packages = [ python poetry pkgs.graphviz ];
             shellHook = ''
-              ln -sfn ${venv} .venv
+              export POETRY_VIRTUALENVS_IN_PROJECT=1
+              ${lib.getExe poetry} env use ${lib.getExe python}
+              ${lib.getExe poetry} install --no-root
             '';
-            packages = [ venv pkgs.poetry ];
           };
         };
     };
