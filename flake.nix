3d44149e110bf3855c595d79c2de4fa8aedad59b
Mirko Lenz <mirko@mirkolenz.com>
2024-02-29 09:54:29 +0100

    chore: apply nixfmt

--- flake.nix
+++ flake.nix
@@ -8,57 +8,64 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs @ {
-    nixpkgs,
-    flake-parts,
-    systems,
-    poetry2nix,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      systems,
+      poetry2nix,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
-      perSystem = {
-        pkgs,
-        system,
-        lib,
-        self',
-        ...
-      }: let
-        python = pkgs.python311;
-        poetry = pkgs.poetry;
-        pkgName = "xarguebuf";
-      in {
-        _module.args.pkgs = import nixpkgs {
-          inherit system;
-          overlays = [poetry2nix.overlays.default];
-        };
-        packages = {
-          default = pkgs.poetry2nix.mkPoetryApplication {
-            inherit python;
-            projectDir = ./.;
-            preferWheels = true;
+      perSystem =
+        {
+          pkgs,
+          system,
+          lib,
+          self',
+          ...
+        }:
+        let
+          python = pkgs.python311;
+          poetry = pkgs.poetry;
+          pkgName = "xarguebuf";
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ poetry2nix.overlays.default ];
           };
-          ${pkgName} = self'.packages.default;
-          dockerImage = pkgs.dockerTools.buildLayeredImage {
-            name = pkgName;
-            tag = "latest";
-            created = "now";
-            config = {
-              entrypoint = [(lib.getExe self'.packages.default)];
-              cmd = ["--help"];
+          packages = {
+            default = pkgs.poetry2nix.mkPoetryApplication {
+              inherit python;
+              projectDir = ./.;
+              preferWheels = true;
+            };
+            ${pkgName} = self'.packages.default;
+            dockerImage = pkgs.dockerTools.buildLayeredImage {
+              name = pkgName;
+              tag = "latest";
+              created = "now";
+              config = {
+                entrypoint = [ (lib.getExe self'.packages.default) ];
+                cmd = [ "--help" ];
+              };
             };
           };
+          devShells.default = pkgs.mkShell {
+            packages = [
+              python
+              poetry
+            ];
+            buildInputs = with pkgs; [ graphviz ];
+            POETRY_VIRTUALENVS_IN_PROJECT = true;
+            shellHook = ''
+              ${lib.getExe poetry} env use ${lib.getExe python}
+              ${lib.getExe poetry} install --no-root --all-extras
+              export BEARER_TOKEN=$(cat bearer-token.txt)
+            '';
+          };
         };
-        devShells.default = pkgs.mkShell {
-          packages = [python poetry];
-          buildInputs = with pkgs; [graphviz];
-          POETRY_VIRTUALENVS_IN_PROJECT = true;
-          shellHook = ''
-            ${lib.getExe poetry} env use ${lib.getExe python}
-            ${lib.getExe poetry} install --no-root --all-extras
-            export BEARER_TOKEN=$(cat bearer-token.txt)
-          '';
-        };
-      };
     };
 }
